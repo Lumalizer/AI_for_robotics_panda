5eac9009e3601b47c297e83f099c460339d3a2cf
Lumalizer <7863320+Lumalizer@users.noreply.github.com>
2024-09-30 10:47:24 +0200

    added threading for camera/gripper

--- read_logs.py
+++ read_logs.py
@@ -12,14 +12,16 @@ for folder in folders:
     logs = pd.read_csv(os.path.join(path, folder, "logs.csv"))
 
     plt.figure()
-    plt.plot(logs['time'] - logs['time'][0], 'o')
+    # plt.plot(logs['time'] - logs['time'][0]), 'o')
+    plt.plot(logs['gripper'], 'o')
     plt.savefig(os.path.join(path, folder, "check_timing.png"))
     trajectories = pickle.load(open(os.path.join(path, folder, "trajectory.pkl"), "rb"))
 
-    print(logs.head())
+    # print(logs.head())
     time, q, dq, poses = trajectories
 
     print(time[:5])
+    print(logs['time'][:5] - logs['time'][0])
 
     # need to sync the gripper with the other logs (use time from both)
     print(len(time))
